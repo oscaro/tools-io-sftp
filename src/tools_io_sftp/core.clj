e4017e518043329669249f08dc85ad9fde731b94
iomonad <iomonad@riseup.net>
2021-12-18 20:40:03 +0100

    added url extractor + spec validator

--- src/tools_io_sftp/core.clj
+++ src/tools_io_sftp/core.clj
@@ -1,28 +1,95 @@
 (ns tools.io.sftp
-  (:require [tools.io.core :refer [register-file-pred!
-                                   mk-input-stream
-                                   list-files]]
-            [clojure.string :as str])
+  (:require [tools.io.core :as tio]
+            [clojure.string :as str]
+            [clojure.spec.alpha :as s])
   (:import [com.jcraft.jsch JSch SftpException ChannelSftp]))
 
-;;;
-;;; Implementation Predicate
-;;;
+;;; specs
+
+(s/def ::username (s/and string? (comp not empty?)))
+(s/def ::password (s/and string? (comp not empty?)))
+(s/def ::port (partial s/int-in-range? 1 65535))
+(s/def ::hostname (s/and string? (comp not empty?)))
+(s/def ::resource (s/and string? (comp not empty?)))
+
+(s/def ::sftp-resource
+  (s/keys :req-un [::hostname
+                   ::port
+                   ::username
+                   ::password
+                   ::resource]))
+
+;;; predicates
 
 (defn- sftp-file?
   [path]
   (str/starts-with? (str/lower-case (str path)) "sftp://"))
 
-(register-file-pred! :sftp sftp-file?)
+(tio/register-file-pred! :sftp sftp-file?)
 
-;;;
-;;; Client / SSH Related
-;;;
+;;; Connection Helpers
 
+(def sftp-uri
+  #"^((sftp:\/\/)(\w*):(\w*)@(\w.*):(\d.*)(\/\w.*))")
 
-;;;
-;;; Method Implementations
-;;;
+(defn extract-uri
+  "Convert url to spec checked map"
+  [target]
+  (when (re-matches sftp-uri target)
+    (let [[u p h po r] (drop 3 (re-find sftp-uri target))
+          res(-> {:username u :password p
+                  :hostname h :port po :resource r}
+                 (update :port read-string))]
+      (if (s/valid? ::sftp-resource res)
+        res
+        (throw (Exception.
+                (s/explain-str ::sftp-resource res)))))))
 
-(defmethod list-files :sftp
-  [url & [options]])
+(defmacro with-ssh-connection
+  "Wrap ssh connection context inside the body
+   then gently close connection after evaluation"
+  [[bname {:keys [username hostname password port] :as spec}]
+   & body]
+  `(let [session# (doto (.getSession (JSch.) ~username ~hostname ~port)
+                    (.setConfig "StrictHostKeyChecking" "no")
+                    (.setConfig "Compression"           "no")
+                    (.setConfig "ControlMaster"         "no")
+                    (.setPassword ~password)
+                    (.connect))
+         ~bname (doto (.openChannel session# "sftp")
+                  (.connect))]
+     (try
+       ~@body
+       (do
+         (.disconnect ~bname)
+         (.disconnect session#)))))
+
+;;; Implementations
+
+(defmethod tio/list-files :sftp
+  [url & [options]]
+  (if-let [conn-spec (extract-uri url)]
+    (with-ssh-connection [conn conn-spec]
+      ;; Do something with
+      )))
+
+(defmethod tio/list-dirs :sftp
+  [url & [options]]
+  (if-let [conn-spec (extract-uri url)]
+    (with-ssh-connection [conn conn-spec]
+      ;; Do something with
+      )))
+
+(defmethod tio/mk-input-stream :sftp
+  [url & [options]]
+  (if-let [conn-spec (extract-uri url)]
+    (with-ssh-connection [conn conn-spec]
+      ;; Do something with
+      )))
+
+(defmethod tio/mk-output-stream :sftp
+  [url & [options]]
+  (if-let [conn-spec (extract-uri url)]
+    (with-ssh-connection [conn conn-spec]
+      ;; Do something with
+      )))
--- test/tools_io_sftp/core_test.clj
+++ test/tools_io_sftp/core_test.clj
@@ -1,7 +1,11 @@
 (ns tools-io-sftp.core-test
   (:require [clojure.test :refer :all]
-            [tools-io-sftp.core :refer :all]))
+            [tools.io.sftp :refer :all]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+(deftest extractor
+  (testing "resourcename extractor"
+    (let [init-1 "sftp://foo:bar@hello.world.fr:22/euoeuoe"
+          rest-1 {:username "foo"
+                  :password "bar" :hostname "hello.world.fr"
+                  :port 22 :resource "/euoeuoe"}]
+      (is (= rest-1 (extract-uri init-1))))))
