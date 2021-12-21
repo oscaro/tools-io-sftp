1b83a31fcc3a9214c48f11c534fc65a6a3e136d7
iomonad <iomonad@riseup.net>
2021-12-21 13:09:10 +0100

    updated implementation

--- dev/user.clj
+++ dev/user.clj
@@ -1,34 +1,15 @@
 (ns user
   (:require [clojure.java.io :as io]
             [tools.io :as tio]
-            [tools.io.sftp :refer [with-ssh-connection]]
+            [tools.io.sftp]
             [clojure.string :as str])
   (:use  [clojure.reflect]
          [clojure.pprint])
   (:import [com.jcraft.jsch JSch SftpException ChannelSftp]
            [java.io OutputStream InputStream]))
 
-;;; Macro context dev
+;;; =======================================================
 
-
-(defn hello-ssh-context []
-  (with-ssh-connection [foo {:password "password"
-                             :username "demo"
-                             :hostname "test.rebex.net"
-                             :port 22}]
-    ;; (pprint (reflect foo))
-    (let [path "/"
-          r (->> (.ls foo path)
-                 (map (fn [e] (.getLongname e)))
-                 (filter #(= \d (first %))))]
-      (->> (map #(str/split % #" ") r)
-           (map last)
-           (remove #(contains? #{"." ".."} %))))))
-
-(defn hello-input-stream []
-  (with-ssh-connection [foo {:password "password"
-                             :username "demo"
-                             :hostname "test.rebex.net"
-                             :port 22}]
-;    (tio/wit)
-    ))
+(comment
+  (tio/list-dirs "sftp://demo:password@test.rebex.net:22/")
+  )
--- src/tools/io/sftp.clj
+++ src/tools/io/sftp.clj
@@ -1,7 +1,10 @@
 (ns tools.io.sftp
-  (:require [tools.io.core :as tio]
+  (:require [tools.io :as utils]
+            [tools.io.core :as tio]
             [clojure.string :as str]
-            [clojure.spec.alpha :as s])
+            [clojure.java.io :as io]
+            [clojure.spec.alpha :as s]
+            [clojure.tools.logging :as log])
   (:import [com.jcraft.jsch JSch SftpException
             ChannelSftp]))
 
@@ -31,37 +34,42 @@
 ;;; Connection Helpers
 
 (def sftp-uri
-  #"^((sftp:\/\/)(\w*):(\w*)@(\w.*):(\d.*)(\/\w.*))")
+  #"^((sftp:\/\/)(\w*):(\w*)@(\w.*):(\d.)(\/(\w.*)?))")
 
 (defn extract-uri
   "Convert url to spec checked map"
   [target]
-  (when (re-matches sftp-uri target)
-    (let [[u p h po r] (drop 3 (re-find sftp-uri target))
-          res(-> {:username u :password p
-                  :hostname h :port po :resource r}
-                 (update :port read-string))]
+  (if (re-matches sftp-uri target)
+    (let [[u p h po r & rest] (drop 3 (re-find sftp-uri target))
+          res (-> {:username u :password p
+                   :hostname h :port po :resource r}
+                  (update :port (fn [e] (when e (Integer/parseInt e)))))]
       (if (s/valid? ::sftp-resource res)
         res
         (throw (Exception.
-                (s/explain-str ::sftp-resource res)))))))
+                (s/explain-str ::sftp-resource res)))))
+    (throw (Exception. "not an sftp url"))))
 
 (defmacro with-ssh-connection
   "Wrap ssh connection context inside the body
    then gently close connection after evaluation"
-  [[bname {:keys [username hostname password port] :as spec}]
-   & body]
-  `(let [session# (doto (.getSession (JSch.) ~username ~hostname ~port)
+  [[bname spec]  & body]
+  `(let [server-spec# ~spec
+         session# (doto (.getSession (JSch.)
+                                     (:username server-spec#)
+                                     (:hostname server-spec#)
+                                     (:port server-spec#))
                     (.setConfig "StrictHostKeyChecking" "no")
                     (.setConfig "Compression"           "no")
                     (.setConfig "ControlMaster"         "no")
-                    (.setPassword ~password)
+                    (.setPassword (:password server-spec#))
                     (.connect))
          ~bname (doto (.openChannel session# "sftp")
                   (.connect))]
      (try
+       (log/debug "sftp channel instanciated with spec" ~spec)
        ~@body
-       (do
+       (finally
          (.disconnect ~bname)
          (.disconnect session#)))))
 
@@ -90,7 +98,8 @@
                      (filter #(= \d (first %))))]
           (->> (map #(str/split % #" ") r)
                (map last)
-               (remove #(contains? #{"." ".."} %))))
+               (remove #(contains? #{"." ".."} %))
+               (map #(str "/" %))))
         (catch Exception e
           nil)))))
 
@@ -99,12 +108,12 @@
   (if-let [conn-spec (extract-uri url)]
     (with-ssh-connection [conn conn-spec]
       ;; TODO
-      )))
+      {:stream nil})))
 
 (defmethod tio/mk-output-stream :sftp
   [url & [options]]
   (if-let [conn-spec (extract-uri url)]
     (with-ssh-connection [conn conn-spec]
-      (tio/with-tempfile [tmp-file]
+      (utils/with-tempfile [tmp-file]
         (.get conn (:resource conn-spec) tmp-file)
-        (io/output-stream tmp-file)))))
+        {:stream (io/output-stream tmp-file)}))))
